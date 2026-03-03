42d72657a45330ccb9718f74c561ebbb2929cdd5
oggunderscore <oggunderscore@gmail.com>
2026-03-03 13:46:15 -0800

    Photos re-route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,7 +35,7 @@
       icon: 'M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z',
       title: 'Photos',
       description: 'Photo & Video Management',
-      url: dev ? 'http://47.141.162.220:2283' : 'https://photos.wkey-industries.net',
+      url: dev ? 'http://47.141.162.220:2283' : '/photos',
       requiresAuth: false,
       showStatus: true
     },
